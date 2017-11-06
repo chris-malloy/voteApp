149b223046520d2fae24b205d31b7f5ab6a906b0
chris-malloy <chrismalloymusic@gmail.com>
2017-11-06 12:22:12 -0500

    added to login and register routes

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,20 @@ const express = require('express');
 const router = express.Router();
 const mysql = require('mysql')
 const config = require('../config/config')
-const connection = mysql.createConnection(config.db);
+const bcrypt = require('bcrypt-nodejs');
 
+const connection = mysql.createConnection(config.db);
+connection.connect((error) => {
+    if (error) {
+        throw error;
+    }
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    if (req.session.name != undefined) {
+        console.log(`Welcome, ${req.session.name}`);
+    }
     var selectQuery = "SELECT * FROM users;";
     connection.query(selectQuery, (error, results) => {
         if (error) {
@@ -26,15 +35,65 @@ router.get("/register", (req, res, next) => {
 })
 
 router.post("/registerProcess", (req, res, next) => {
-    res.render('register', {});
+    var name = req.body.name;
+    var email = req.body.email;
+    var password = req.body.password;
+    // ensure user email does not exists
+    const selectQuery = `SELECT * FROM users WHERE email = ?;`;
+    connection.query(selectQuery, [email], (error, results) => {
+        if (results.length != 0) {
+            res.redirect('/register?msg=registered')
+        } else {
+            var hash = bcrypt.hashSync(password);
+            insertQuery = `INSERT INTO users (name,email,password) VALUES (?,?,?);`;
+            connection.query(insertQuery, [name, email, hash], (error) => {
+                if (error) {
+                    throw error;
+                } else {
+                    res.redirect('/registerSuccess?msg=registered');
+                };
+            });
+        };
+    });
 })
+
+router.get('/registerSuccess'), (req, res, next) => {
+    res.render('registerSuccess');
+}
 
 /* Login */
 router.get("/login", (req, res, next) => {
     res.render("login", {});
 })
 router.post("/loginProcess", (req, res, next) => {
-    res.render("login", {});
+    var email = req.body.email;
+    var password = req.body.password;
+    var selectQuery = `SELECT * FROM users WHERE email = ?;`;
+    connection.query(selectQuery, [email], (error, results) => {
+        if (error) {
+            throw error;
+        } else {
+            if (results.length == 0) {
+                res.redirect('/login?msg=userNotFound');
+            } else {
+                // password = user pass, results[0].password = hash saved to database
+                var passwordsMatch = bcrypt.compareSync(password, results[0].password);
+                if (passwordsMatch) {
+                    req.session.name = results[0].name;
+                    req.session.id = results[0].id;
+                    req.session.email = results[0].email;
+                    res.redirect('/');
+                } else {
+                    res.redirect('/login?msg=badPass');
+                }
+            };
+        };
+    });
+});
+
+/* Standings */
+router.get("/standings", (req, res, next) => {
+    res.render("standings", {});
 })
 
 module.exports = router;
