3416881bd690766f8ffc865c86a01982057457a6
chris-malloy <chrismalloymusic@gmail.com>
2017-11-06 09:53:34 -0500

    set up database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql')
+const config = require('../config/config')
+const connection = mysql.createConnection(config.db);
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'VotIT' });
+    var selectQuery = "SELECT * FROM users;";
+    connection.query(selectQuery, (error, results) => {
+        if (error) {
+            throw error;
+        } else {
+            res.render('index', {
+                title: 'VotIT',
+                users: results
+            });
+        }
+    })
 });
 
 module.exports = router;
