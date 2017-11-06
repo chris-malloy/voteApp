732965221fc0b165856400c3473cf0c1be89fa35
chris-malloy <chrismalloymusic@gmail.com>
2017-11-06 10:36:07 -0500

    added routes

--- routes/index.js
+++ routes/index.js
@@ -13,11 +13,28 @@ router.get('/', function(req, res, next) {
             throw error;
         } else {
             res.render('index', {
-                title: 'VotIT',
+                title: 'Rock or Not',
                 users: results
             });
         }
     })
 });
+
+/* REGISTER */
+router.get("/register", (req, res, next) => {
+    res.render('register', {});
+})
+
+router.post("/registerProcess", (req, res, next) => {
+    res.render('register', {});
+})
+
+/* Login */
+router.get("/login", (req, res, next) => {
+    res.render("login", {});
+})
+router.post("/loginProcess", (req, res, next) => {
+    res.render("login", {});
+})
 
 module.exports = router;
