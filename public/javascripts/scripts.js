5c4e668a80d8cf58562f0dbcd12906ad6b98ed64
chris-malloy <chrismalloymusic@gmail.com>
2017-11-07 17:24:49 -0500

    multipart/form-data

---
+++ public/javascripts/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(() => {
+    var $grid = $('.grid').isotope({});
+    $('#top-rated').click(() => {
+        $grid.isotope({ filter: '.transition' })
+    });
+    $('#worst-rated').click(() => {
+        $grid.isotope({ filter: 'worst-rated' })
+    })
+    $('#all-bands').click(() => {
+        $grid.isotope({ filter: '' })
+    })
+});
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,13 @@ const mysql = require('mysql')
 const config = require('../config/config')
 const bcrypt = require('bcrypt-nodejs');
 
+const fs = require('fs');
+const multer = require('multer');
+const uploadDir = multer({
+    dest: 'public/images'
+});
+const nameOfFileField = uploadDir.single('imageToUpload');
+
 const connection = mysql.createConnection(config.db);
 connection.connect((error) => {
     if (error) {
@@ -13,32 +20,32 @@ connection.connect((error) => {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    if (req.session.name == undefined) {
-        res.redirect('/login?msg=mustLogin');
-        return;
-    }
-    const getBands = new Promise((resolve, reject) => {
-        var selectQuery = "SELECT * FROM bands;";
-        connection.query(selectQuery, (error, results, fields) => {
-            if (error) {
-                reject(error)
+        if (req.session.name === undefined) {
+            res.redirect('/login?msg=mustlogin')
+            return;
+        }
+        //we want to select all the image that the user has not voted on
+        var selectQuery = `
+        SELECT * FROM images WHERE id NOT IN(
+            SELECT imgID FROM votes WHERE userID = ?
+        );
+    `
+            // var selectQuery = 'SELECT * FROM images;';
+        connection.query(selectQuery, [req.session.uid], (error, results, field) => {
+            var rand = Math.floor(Math.random() * results.length);
+            if (results.length == 0) {
+                res.redirect('/standings')
             } else {
-                var rand = Math.floor(Math.random() * results.length);
-                resolve(results[rand]);
+                res.render('index', {
+                    results: results[rand],
+                    name: req.session.name,
+                    loggedIn: true
+                });
             }
-        });
-    });
-    getBands.then((bandObj) => {
-        console.log(bandObj);
-        res.render('index', {
-            title: 'Rock or Not',
-            name: req.session.name,
-            band: bandObj
-        });
-    })
-});
+        })
 
-/* REGISTER */
+    })
+    /* REGISTER */
 router.get("/register", (req, res, next) => {
     res.render('register', {});
 })
@@ -53,21 +60,17 @@ router.post("/registerProcess", (req, res, next) => {
             res.redirect('/register?msg=registered')
         } else {
             var hash = bcrypt.hashSync(password);
-            insertQuery = `INSERT INTO users (name,email,password) VALUES (?,?,?);`;
+            var insertQuery = `INSERT INTO users (name,email,password) VALUES (?,?,?);`;
             connection.query(insertQuery, [name, email, hash], (error) => {
                 if (error) {
                     throw error;
                 } else {
-                    res.redirect('/registerSuccess?msg=registered');
+                    res.redirect('/?msg=welcome');
                 };
             });
         };
     });
 })
-
-router.get('/registerSuccess'), (req, res, next) => {
-    res.render('registerSuccess');
-}
 
 /* Login */
 router.get("/login", (req, res, next) => {
@@ -100,6 +103,11 @@ router.post("/loginProcess", (req, res, next) => {
     });
 });
 
+router.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/login');
+})
+
 router.get('/vote/:direction/:bandId', (req, res, next) => {
     var bandId = req.params.bandId;
     var direction = req.params.direction;
@@ -116,8 +124,60 @@ router.get('/vote/:direction/:bandId', (req, res, next) => {
 
 
 /* Standings */
-router.get("/standings", (req, res, next) => {
-    res.render("standings", {});
+router.get('/standings', (req, res, next) => {
+    const standingsQuery = `
+        SELECT bands.title,bands.imageUrl,votes.imageID, SUM(IF(voteDirection='up',1,0)) as upVotes, SUM(IF(voteDirection='down',1,0)) as downVotes, SUM(IF(voteDirection='up',1,-1)) as total FROM votes
+            INNER JOIN bands on votes.imageID = bands.id
+            GROUP BY imageID;`
+    connection.query(standingsQuery, (error, results) => {
+        results.map((band, i) => {
+            if (band.upVotes / (band.upVotes + band.downVotes) > .8) {
+                results[i].class = "top-rated"
+            } else if (band.upVotes / (band.upVotes + band.downVotes) <= .5) {
+                results[i].cls = "worst-rated";
+            } else {
+                results[i].cls = "middle";
+            }
+        });
+        if (error) {
+            throw error;
+        } else {
+            res.render('standings', {
+                standingsResults: results
+            });
+        }
+    })
+})
+
+router.post('/uploadBand', (req, res) => {
+    res.render('upload');
+})
+router.post('/formSubmit', nameOfFileField, (req, res) => {
+    var tmpPath = req.file.path;
+    var targetPath = `public/images/${req.file.originalname}`;
+    fs.readFile(tmpPath, (error, fileContents) => {
+        if (error) {
+            throw error;
+        }
+        fs.writeFile(targetPath, fileContents, (error) => {
+            if (error) {
+                throw error;
+            }
+            var insertQuery = `INSERT INTO bands (imageUrl, title) 
+                                    VALUES
+                                    (?,?);`
+            connection.query(insertQuery, [req.file.originalname, req.body.bandName], (error) => {
+                if (dbError) {
+                    throw dbError;
+                }
+                res.redirect('/')
+            })
+        })
+    })
 })
 
 module.exports = router;
+
+// TODO - 
+// fix login bool
+// fix login
